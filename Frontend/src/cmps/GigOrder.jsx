9b510901e4afcce19a86b9ddd1c70e451ccce91d
RoeiTal123 <roeit1977@gmail.com>
2023-10-03 21:00:03 +0300

    added continue button

--- Frontend/src/cmps/GigOrder.jsx
+++ Frontend/src/cmps/GigOrder.jsx
@@ -1,8 +1,14 @@
 import clock from "../assets/img/svg/clock.icon.svg"
 import refresh from "../assets/img/svg/refresh.icon.svg"
 import checkmark from "../assets/img/svg/checkmark.icon.svg"
+import arrow from "../assets/img/svg/arrow.icon.svg"
 
 export function GigOrder() {
+
+    function onContinue(){
+        console.log('continue!')
+    }
+
   return (
     <section className="gig-order">
       <div className="title">
@@ -14,42 +20,48 @@ export function GigOrder() {
       </p>
       <div className="shipping-info">
         <div className="inside-shipping">
-          <img src={clock} />
+          <img src={clock}/>
           <span> 1 Days Delivery</span>
         </div>
         <div className="inside-shipping">
-          <img src={refresh} />
+          <img src={refresh}/>
           <span>Unlimited Revisions</span>
         </div>
       </div>
       <ul>
         <li>
-          <img src={checkmark} />
+          <img src={checkmark}/>
           1 concept included
           </li>
         <li>
-          <img src={checkmark} />
+          <img src={checkmark}/>
           Logo transparency
           </li>
         <li>
-          <img src={checkmark} />
+          <img src={checkmark}/>
           Vector file
           </li>
         <li>
-          <img src={checkmark} />Printable file</li>
+          <img src={checkmark}/>
+          Printable file</li>
         <li>
-          <img src={checkmark} />
+          <img src={checkmark}/>
           Include 3D mockup
           </li>
         <li>
-          <img src={checkmark} />
+          <img src={checkmark}/>
           Include source file
           </li>
         <li>
-          <img src={checkmark} />
+          <img src={checkmark}/>
           Include social media kit
           </li>
       </ul>
+      <div className="continue">
+      <button onClick={onContinue}>
+        Continue <img src={arrow}/>
+      </button>
+      </div>
     </section>
   )
 }
