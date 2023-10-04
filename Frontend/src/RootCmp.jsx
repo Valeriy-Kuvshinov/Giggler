5b284c9be0c5caf164bd93f77eb31b4e11b3f392
Valeriy-Kuvshinov <valerykuvshinuv@gmail.com>
2023-10-04 10:24:16 +0300

    added routes to explore and gig details, adjusted header

--- Frontend/src/RootCmp.jsx
+++ Frontend/src/RootCmp.jsx
@@ -6,20 +6,25 @@ import routes from './routes'
 import { AppHeader } from './cmps/AppHeader'
 import { AppFooter } from './cmps/AppFooter'
 import { UserDetails } from './pages/UserDetails'
+import { GigIndex } from './pages/GigIndex'
+import { GigDetails } from './pages/GigDetails'
+import { HomePage } from './pages/HomePage'
 
 export function RootCmp() {
-
     return (
-        <div>
+        <main className='main-container'>
             <AppHeader />
-            <main>
+            <main className='full'>
                 <Routes>
                     {routes.map(route => <Route key={route.path} exact={true} element={route.component} path={route.path} />)}
+                    <Route path="/" element={<HomePage />} />
                     <Route path="user/:id" element={<UserDetails />} />
+                    <Route path="explore/" element={<GigIndex />} />
+                    <Route path="gigdetails/" element={<GigDetails />} />
                 </Routes>
             </main>
             <AppFooter />
-        </div>
+        </main>
     )
 }
 
