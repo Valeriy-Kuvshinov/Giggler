e2859a5aa868dfe24d6ec22b51bc7f6df240e618
RoeiTal123 <roeit1977@gmail.com>
2023-10-04 12:11:39 +0300

    .

--- Frontend/src/pages/GigDetails.jsx
+++ Frontend/src/pages/GigDetails.jsx
@@ -4,12 +4,23 @@ import { AboutSeller } from "../cmps/AboutSeller"
 import { GigOrder } from "../cmps/GigOrder"
 import { GigReviews } from "../cmps/GigReviews"
 
+import { gigService } from "../services/gig.service.local"
+
+import { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+
 export function GigDetails() {
+
+   const params = useParams()
+   const { gig, setGig } = useState({})
+
+   useEffect(()=>{
+    console.log(params._id)
+      console.log(gigService.query())
+   },[])
+
   return (
-    <section 
-    // className="gig-details"
-    className="gig-details main-container full"
-    >
+    <section className="gig-details main-container full">
       <section className="gig">
       <div className="gig-info">
       <GigHeader/>
