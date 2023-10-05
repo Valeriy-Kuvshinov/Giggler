1f34c67e462f3180966e8b013148cd345ece4b4e
Valeriy-Kuvshinov <valerykuvshinuv@gmail.com>
2023-10-05 09:56:00 +0300

    quick code adjustments

--- Frontend/src/cmps/GigOrder.jsx
+++ Frontend/src/cmps/GigOrder.jsx
@@ -3,11 +3,10 @@ import refresh from "../assets/img/svg/refresh.icon.svg"
 import checkmark from "../assets/img/svg/checkmark.icon.svg"
 import arrow from "../assets/img/svg/arrow.icon.svg"
 
-export function GigOrder({gig}) {
-
-    function onContinue(){
-        console.log('continue!')
-    }
+export function GigOrder({ gig }) {
+  function onContinue() {
+    console.log('continue!')
+  }
 
   return (
     <section className="gig-order">
@@ -20,47 +19,47 @@ export function GigOrder({gig}) {
       </p>
       <div className="shipping-info">
         <div className="inside-shipping">
-          <img src={clock}/>
+          <img src={clock} />
           <span> {gig.daysToMake} Days Delivery</span>
         </div>
         <div className="inside-shipping">
-          <img src={refresh}/>
+          <img src={refresh} />
           <span>Unlimited Revisions</span>
         </div>
       </div>
       <ul>
         <li>
-          <img src={checkmark}/>
+          <img src={checkmark} />
           1 concept included
-          </li>
+        </li>
         <li>
-          <img src={checkmark}/>
+          <img src={checkmark} />
           Logo transparency
-          </li>
+        </li>
         <li>
-          <img src={checkmark}/>
+          <img src={checkmark} />
           Vector file
-          </li>
+        </li>
         <li>
-          <img src={checkmark}/>
+          <img src={checkmark} />
           Printable file</li>
         <li>
-          <img src={checkmark}/>
+          <img src={checkmark} />
           Include 3D mockup
-          </li>
+        </li>
         <li>
-          <img src={checkmark}/>
+          <img src={checkmark} />
           Include source file
-          </li>
+        </li>
         <li>
-          <img src={checkmark}/>
+          <img src={checkmark} />
           Include social media kit
-          </li>
+        </li>
       </ul>
       <div className="continue">
-      <button onClick={onContinue}>
-        Continue <img src={arrow}/>
-      </button>
+        <button onClick={onContinue}>
+          Continue <img src={arrow} />
+        </button>
       </div>
     </section>
   )
