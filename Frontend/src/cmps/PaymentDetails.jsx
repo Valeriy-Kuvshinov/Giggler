6bf0cda42b06281b97a6002a8f35b091b33a1939
RoeiTal123 <roeit1977@gmail.com>
2023-10-06 15:31:46 +0300

    final payment page design change

--- Frontend/src/cmps/PaymentDetails.jsx
+++ Frontend/src/cmps/PaymentDetails.jsx
@@ -1,32 +1,36 @@
+import SvgIcon from './SvgIcon'
+import visa from '../assets/img/svg/visa.icon.svg'
 
 export function PaymentDetails(){
-    return (<section>
+    return (<section className='payment-details'>
         <h2>Payment Option</h2>
-        <div>
+        <div className='credit-type'>
             <span>Credit & Debit Cards</span>
-            <img/>
+            <img className="visa" src={visa}/>
         </div>
-        <div>
+        <div className='credit-details'>
+        <div className='credit-number'>
             <span>Card Number</span>
             <input type="text" id="crdNum" placeholder="XXXX XXXX XXXX XXXX"/>
         </div>
-        <div>
-            <div>
+        <div className='rest-of-details'>
+            <div className='credit-date'>
                 <span>Expiration Date</span>
-                <input type='date' id="expDate"/>
+                <input type='date' id="expDate" />
             </div>
-            <div>
+            <div className='credit-date'>
                 <span>Security Code</span>
                 <input type='number' id="pinCode" placeholder="XXX"/>
             </div>
         </div>
-        <div>
+        <div className='buyer-info'>
             <span>First Name</span>
-            <input type="text" id="firstName" placeholder="first name"/>
+            <input type="text" id="firstName"/>
         </div>
-        <div>
+        <div className='buyer-info'>
             <span>Last Name</span>
-            <input type="text" id="LastName" placeholder="last name"/>
+            <input type="text" id="LastName"/>
+        </div>
         </div>
         </section>)
 }
