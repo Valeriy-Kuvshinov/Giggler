d7d9f6af8665795de2ae1bef5beada825bde6849
Valeriy-Kuvshinov <valerykuvshinuv@gmail.com>
2023-10-10 01:27:50 +0300

    login-signup form remade

--- Frontend/src/cmps/LoginSignup.jsx
+++ Frontend/src/cmps/LoginSignup.jsx
@@ -2,7 +2,8 @@ import React, { useRef, useState, useEffect } from 'react'
 import { ImgUploader } from './ImgUploader.jsx'
 import { login, signup, loadUsers } from '../store/user.actions.js'
 import { showErrorMsg } from '../services/event-bus.service.js'
-import loginSignupPicture from '../assets/img/login-signup.png'
+import loginSignupImg from '../assets/img/login-signup.png'
+import checkmarkImg from '../assets/img/svg/checkmark.icon.svg'
 
 export function LoginSignup({ closeModal, mode }) {
     const [credentials, setCredentials] = useState({ username: '', password: '', fullname: '' })
@@ -72,8 +73,24 @@ export function LoginSignup({ closeModal, mode }) {
 
     return (
         <main className="user-login-wrapper" ref={wrapperRef}>
-            <section className='login-notes flex column'>
-                <img src={loginSignupPicture} />
+            <section className='login-notes flex column' style={{ backgroundImage: `url("${loginSignupImg}")` }}>
+                <div className='text'>
+                    <h2>Success starts here</h2>
+                    <ul>
+                        <li className='flex row'>
+                            <img src={checkmarkImg} />
+                            <h3>Over 600 categories</h3>
+                        </li>
+                        <li className='flex row'>
+                            <img src={checkmarkImg} />
+                            <h3>Pay per project, not per hour</h3>
+                        </li>
+                        <li className='flex row'>
+                            <img src={checkmarkImg} />
+                            <h3>Access to talent and businesses across the globe</h3>
+                        </li>
+                    </ul>
+                </div>
             </section>
             <section className='user-interaction flex column'>
                 {isSignup ? (
@@ -87,58 +104,82 @@ export function LoginSignup({ closeModal, mode }) {
                         <h3>Don’t have an account? <span onClick={() => setIsSignup(true)}>Join here</span></h3>
                     </>
                 )}
-                <form onSubmit={isSignup ? handleSignup : handleLogin}>
+                <form className='flex column' onSubmit={isSignup ? handleSignup : handleLogin}>
                     {!isSignup && (
                         <>
-                            <input
-                                type="text"
-                                name="username"
-                                value={credentials.username}
-                                placeholder="Username"
-                                onChange={handleChange}
-                                required
-                                autoFocus
-                            />
-                            <input
-                                type="password"
-                                name="password"
-                                value={credentials.password}
-                                placeholder="Password"
-                                onChange={handleChange}
-                                required
-                            />
+                            <div className="login-section flex column">
+                                <div className='flex column'>
+                                    <label htmlFor="login-username">Username</label>
+                                    <input
+                                        id="login-username"
+                                        type="text"
+                                        name="username"
+                                        value={credentials.username}
+                                        placeholder="Username"
+                                        onChange={handleChange}
+                                        required
+                                        autoFocus
+                                    />
+                                </div>
+                                <div className='flex column'>
+                                    <label htmlFor="login-password">Password</label>
+                                    <input
+                                        id="login-password"
+                                        type="password"
+                                        name="password"
+                                        value={credentials.password}
+                                        placeholder="Password"
+                                        onChange={handleChange}
+                                        required
+                                    />
+                                </div>
+                            </div>
                         </>
                     )}
                     {isSignup && (
-                        <div className="signup-section">
-                            <input
-                                type="text"
-                                name="fullname"
-                                value={credentials.fullname}
-                                placeholder="Fullname"
-                                onChange={handleChange}
-                                required
-                            />
-                            <input
-                                type="text"
-                                name="username"
-                                value={credentials.username}
-                                placeholder="Username"
-                                onChange={handleChange}
-                                required
-                            />
-                            <input
-                                type="password"
-                                name="password"
-                                value={credentials.password}
-                                placeholder="Password"
-                                onChange={handleChange}
-                                required
-                            />
+                        <div className="signup-section flex column">
+                            <div className='flex column'>
+                                <label htmlFor="signup-fullname">Full Name</label>
+                                <input
+                                    id="signup-fullname"
+                                    type="text"
+                                    name="fullname"
+                                    value={credentials.fullname}
+                                    placeholder="Fullname"
+                                    onChange={handleChange}
+                                    required
+                                />
+                            </div>
+                            <div className='flex column'>
+                                <label htmlFor="signup-username">Username</label>
+                                <input
+                                    id="signup-username"
+                                    type="text"
+                                    name="username"
+                                    value={credentials.username}
+                                    placeholder="Username"
+                                    onChange={handleChange}
+                                    required
+                                />
+                            </div>
+                            <div className='flex column'>
+                                <label htmlFor="signup-password">Password</label>
+                                <input
+                                    id="signup-password"
+                                    type="password"
+                                    name="password"
+                                    value={credentials.password}
+                                    placeholder="Password"
+                                    onChange={handleChange}
+                                    required
+                                />
+                            </div>
+
                             <ImgUploader onUploaded={onUploaded} />
                         </div>
                     )}
                     <button type="submit">Continue</button>
+                    <p>By joining, you agree to the Fiverr Terms of Service and to occasionally receive emails from us. Please read our Privacy Policy to learn how we use your personal data.</p>
                 </form>
             </section>
         </main>
