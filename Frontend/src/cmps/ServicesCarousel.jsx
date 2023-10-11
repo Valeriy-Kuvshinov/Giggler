3de76c45cf73d737cf1faa02e11a7a5fa910099b
Valeriy-Kuvshinov <valerykuvshinuv@gmail.com>
2023-10-11 15:45:19 +0300

    added another section to home page

---
+++ Frontend/src/cmps/ServicesCarousel.jsx
@@ -0,0 +1,72 @@
+import React, { useState, useEffect } from 'react'
+import leftArrowSvg from '../assets/img/svg/left.side.icon.svg'
+import rightArrowSvg from '../assets/img/svg/right.side.icon.svg'
+import { galleryService } from '../services/gallery.service.js'
+
+export function ServicesCarousel() {
+    const { serviceImages, serviceTexts } = galleryService
+    const [visibleStartIndex, setVisibleStartIndex] = useState(0)
+    const [windowWidth, setWindowWidth] = useState(window.innerWidth)
+
+    useEffect(() => {
+        const handleResize = () => {
+            setWindowWidth(window.innerWidth)
+        }
+        window.addEventListener('resize', handleResize)
+        return () => window.removeEventListener('resize', handleResize)
+    }, [])
+
+    let itemsPerPage = 5
+    if (windowWidth < 1300) itemsPerPage = 4
+    if (windowWidth < 1100) itemsPerPage = 3
+    if (windowWidth < 800) itemsPerPage = 2
+    if (windowWidth < 600) itemsPerPage = 1
+
+    function scrollServicesCarousel(direction) {
+        if (direction === 'left') {
+            if (visibleStartIndex - itemsPerPage < 0) {
+                setVisibleStartIndex(serviceImages.length - itemsPerPage)
+            }
+            else setVisibleStartIndex(visibleStartIndex - itemsPerPage)
+
+        } else if (direction === 'right') {
+            if (visibleStartIndex + itemsPerPage >= serviceImages.length) {
+                setVisibleStartIndex(0)
+            }
+            else setVisibleStartIndex(visibleStartIndex + itemsPerPage)
+        }
+    }
+
+    function getCurrentDisplayItems() {
+        let items = []
+        for (let i = 0; i < itemsPerPage; i++) {
+            items.push(serviceImages[(visibleStartIndex + i) % serviceImages.length])
+        }
+        return items
+    }
+    const displayItems = getCurrentDisplayItems()
+
+    return (
+        <section className='home-services-section'>
+            <h2>Popular services</h2>
+            <button className="carousel-btn left-side" onClick={() => scrollServicesCarousel('left')}>
+                <img src={leftArrowSvg} alt="Left Arrow" />
+            </button>
+            <div className='services flex row'>
+                {displayItems.map((service, index) => (
+                    <div className="service" key={index}>
+                        <img src={service} alt={`Service image ${index}`} />
+                        <h4 className="service-text">
+                            <span>{serviceTexts[(visibleStartIndex + index) % serviceTexts.length].title}</span>
+                            <br />
+                            {serviceTexts[(visibleStartIndex + index) % serviceTexts.length].subtitle}
+                        </h4>
+                    </div>
+                ))}
+            </div>
+            <button className="carousel-btn right-side" onClick={() => scrollServicesCarousel('right')}>
+                <img src={rightArrowSvg} alt="Right Arrow" />
+            </button>
+        </section>
+    )
+}
--- Frontend/src/pages/HomePage.jsx
+++ Frontend/src/pages/HomePage.jsx
@@ -1,51 +1,10 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import { galleryService } from '../services/gallery.service.js'
 import { WelcomeSection } from '../cmps/WelcomeSection.jsx'
-import leftArrowSvg from '../assets/img/svg/left.side.icon.svg'
-import rightArrowSvg from '../assets/img/svg/right.side.icon.svg'
+import { ServicesCarousel } from '../cmps/ServicesCarousel.jsx'
 
 export function HomePage() {
-    const { companyImages, serviceImages, categoryIcons, serviceTexts, categoryTexts } = galleryService
-    const [visibleStartIndex, setVisibleStartIndex] = useState(0)
-    const [windowWidth, setWindowWidth] = useState(window.innerWidth)
-
-    useEffect(() => {
-        const handleResize = () => {
-            setWindowWidth(window.innerWidth)
-        }
-        window.addEventListener('resize', handleResize)
-        return () => window.removeEventListener('resize', handleResize)
-    }, [])
-
-    let itemsPerPage = 5
-    if (windowWidth < 1300) itemsPerPage = 4
-    if (windowWidth < 1100) itemsPerPage = 3
-    if (windowWidth < 800) itemsPerPage = 2
-    if (windowWidth < 600) itemsPerPage = 1
-
-    function scrollServicesCarousel(direction) {
-        if (direction === 'left') {
-            if (visibleStartIndex - itemsPerPage < 0) {
-                setVisibleStartIndex(serviceImages.length - itemsPerPage)
-            }
-            else setVisibleStartIndex(visibleStartIndex - itemsPerPage)
-
-        } else if (direction === 'right') {
-            if (visibleStartIndex + itemsPerPage >= serviceImages.length) {
-                setVisibleStartIndex(0)
-            }
-            else setVisibleStartIndex(visibleStartIndex + itemsPerPage)
-        }
-    }
-
-    function getCurrentDisplayItems() {
-        let items = []
-        for (let i = 0; i < itemsPerPage; i++) {
-            items.push(serviceImages[(visibleStartIndex + i) % serviceImages.length])
-        }
-        return items
-    }
-    const displayItems = getCurrentDisplayItems()
+    const { companyImages, categoryIcons, categoryTexts } = galleryService
 
     return (
         <section className='home-wrapper'>
@@ -58,27 +17,28 @@ export function HomePage() {
                     ))}
                 </div>
             </section>
-            <section className='home-services-section'>
-                <h2>Popular services</h2>
-                <button className="carousel-btn left-side" onClick={() => scrollServicesCarousel('left')}>
-                    <img src={leftArrowSvg} alt="Left Arrow" />
-                </button>
-                <div className='services flex row'>
-                    {displayItems.map((service, index) => (
-                        <div className="service" key={index}>
-                            <img src={service} alt={`Service image ${index}`} />
-                            <h4 className="service-text">
-                                <span>{serviceTexts[(visibleStartIndex + index) % serviceTexts.length].title}</span>
-                                <br />
-                                {serviceTexts[(visibleStartIndex + index) % serviceTexts.length].subtitle}
-                            </h4>
-                        </div>
-                    ))}
-                </div>
-                <button className="carousel-btn right-side" onClick={() => scrollServicesCarousel('right')}>
-                    <img src={rightArrowSvg} alt="Right Arrow" />
-                </button>
+            <section className='notes-section'>
+                <h2>The best part? Everything.</h2>
+                <ul>
+                    <li>
+                        <h3>Stick to your budget</h3>
+                        <p>Find the right service for every price point. No hourly rates, just project-based pricing.</p>
+                    </li>
+                    <li>
+                        <h3>Get quality work done quickly</h3>
+                        <p>Hand your project over to a talented freelancer in minutes, get long-lasting results.</p>
+                    </li>
+                    <li>
+                        <h3>Pay when you're happy</h3>
+                        <p>Upfront quotes mean no surprises. Payments only get released when you approve.</p>
+                    </li>
+                    <li>
+                        <h3>Count on 24/7 support</h3>
+                        <p>Our round-the-clock support team is available to help anytime, anywhere.</p>
+                    </li>
+                </ul>
             </section>
+            <ServicesCarousel />
             <section className='home-categories-section'>
                 <h2>You need it, we've got it</h2>
                 <div className='categories grid'>
