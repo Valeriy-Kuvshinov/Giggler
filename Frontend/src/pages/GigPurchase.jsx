f2f9bd46e3bbf6d00b2ec19a2e8bc2dde964e144
RoeiTal123 <roeit1977@gmail.com>
2023-10-11 16:00:15 +0300

    purchase page is refreshable and working

--- Frontend/src/pages/GigPurchase.jsx
+++ Frontend/src/pages/GigPurchase.jsx
@@ -1,25 +1,43 @@
+import { useState , useEffect } from "react"
 import { useParams } from "react-router-dom"
 import { useSelector } from 'react-redux'
 
 import { PaymentDetails } from "../cmps/PaymentDetails.jsx"
 import { PaymentInfo } from "../cmps/PaymentInfo.jsx"
 
-import { orderService } from "../services/order.service.local"
-import { orderBackendService } from "../services/order.backend.service.js"
 import {showErrorMsg,showSuccessMsg} from '../services/event-bus.service.js'
+import { orderBackendService } from "../services/order.backend.service.js"
+import { gigBackendService } from "../services/gig.backend.service.js"
+import { userService } from "../services/user.service.js"
 
 import { saveOrder } from "../store/order.actions.js"
 
 export function GigPurchase() {
-  const user = useSelector(storeState => storeState.userModule.user)
-  var gigs = useSelector(storeState => storeState.gigModule.gigs)
   const params = useParams()
-  var gig=gigs.find((gig)=>gig._id===params.id)
+  const [gig,setGig]=useState(null)
+  const [user,setUser]=useState(null)
 
-  // function createOrder(){
-  //   const order=orderService.createOrder(user._id,user.fullName,gig.owner._id,gig._id,gig.price)
-  //   orderService.save(order)
-  // }
+  useEffect(()=>{
+    loadGig()
+    loadUser()
+  },[])
+
+  async function loadGig(){
+    try{
+      const gig=await gigBackendService.getById(params.id)
+      setGig(gig)
+    } catch (err){
+      console.log('couldnt load gig : ',err)
+    }
+  }
+  async function loadUser(){
+    try{
+      const user=await userService.getLoggedinUser()
+      setUser(user)
+    } catch (err){
+      console.log('couldnt load user : ',err)
+    }
+  }
 
   function createOrder() {
     const orderToSave = orderBackendService.
@@ -35,7 +53,9 @@ export function GigPurchase() {
         })
   }
 
-  // console.log(gig)
+  if(gig===null||user===null) return <div>loading...</div>
+
+  // console.log('gig : ',gig)
   
   return (
     <section className="main-container">
