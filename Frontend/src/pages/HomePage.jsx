9efe2d3e9c52702bc2a784f072ccb9866b18e7af
Valeriy-Kuvshinov <valerykuvshinuv@gmail.com>
2023-10-13 00:52:11 +0300

    final section added to homePage

--- Frontend/src/pages/HomePage.jsx
+++ Frontend/src/pages/HomePage.jsx
@@ -2,11 +2,12 @@ import React from 'react'
 import { galleryService } from '../services/gallery.service.js'
 import { WelcomeSection } from '../cmps/WelcomeSection.jsx'
 import { ServicesCarousel } from '../cmps/ServicesCarousel.jsx'
+import { InfoListItem } from '../cmps/InfoListItem.jsx'
 import customCheckmarkImg from '../assets/img/svg/special.checkmark.icon.svg'
 import infoImg from '../assets/img/fiverr-show.webp'
 
 export function HomePage() {
-    const { companyImages, categoryIcons, categoryTexts } = galleryService
+    const { companyImages, categoryIcons, categoryTexts, infoListData } = galleryService
 
     return (
         <section className='home-wrapper'>
@@ -27,39 +28,20 @@ export function HomePage() {
                     <section className='text'>
                         <h2>The best part? Everything.</h2>
                         <ul>
-                            <li>
-                                <div className='flex row'>
-                                    <img src={customCheckmarkImg} />
-                                    <h3>Stick to your budget</h3>
-                                </div>
-                                <p>Find the right service for every price point. No hourly rates, just project-based pricing.</p>
-                            </li>
-                            <li>
-                                <div className='flex row'>
-                                    <img src={customCheckmarkImg} />
-                                    <h3>Get quality work done quickly</h3>
-                                </div>
-                                <p>Hand your project over to a talented freelancer in minutes, get long-lasting results.</p>
-                            </li>
-                            <li>
-                                <div className='flex row'>
-                                    <img src={customCheckmarkImg} />
-                                    <h3>Pay when you're happy</h3>
-                                </div>
-                                <p>Upfront quotes mean no surprises. Payments only get released when you approve.</p>
-                            </li>
-                            <li>
-                                <div className='flex row'>
-                                    <img src={customCheckmarkImg} />
-                                    <h3>Count on 24/7 support</h3>
-                                </div>
-                                <p>Our round-the-clock support team is available to help anytime, anywhere.</p>
-                            </li>
+                            {infoListData.map((item, idx) => (
+                                <InfoListItem
+                                    key={idx}
+                                    imgSrc={customCheckmarkImg}
+                                    title={item.title}
+                                    description={item.description}
+                                />
+                            ))}
                         </ul>
                     </section>
-                    <img src={infoImg} />
+                    <img className='info-img' src={infoImg} />
                 </section>
             </div>
+
             <section className='home-categories-section'>
                 <h2>You need it, we've got it</h2>
                 <div className='categories grid'>
@@ -71,6 +53,13 @@ export function HomePage() {
                     ))}
                 </div>
             </section>
+
+            <div className='join-wrapper'>
+                <section className='join-section'>
+                    <h2>Suddenly it's all so <span>doable</span>.</h2>
+                    <button>Join Fiverr</button>
+                </section>
+            </div>
         </section>
     )
 }
