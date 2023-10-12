98b1e04af4c09de97541caf29d2ed45637a48171
Valeriy-Kuvshinov <valerykuvshinuv@gmail.com>
2023-10-12 22:55:36 +0300

    new section added to Home page

--- Frontend/src/pages/HomePage.jsx
+++ Frontend/src/pages/HomePage.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { galleryService } from '../services/gallery.service.js'
 import { WelcomeSection } from '../cmps/WelcomeSection.jsx'
 import { ServicesCarousel } from '../cmps/ServicesCarousel.jsx'
+import customCheckmarkImg from '../assets/img/svg/special.checkmark.icon.svg'
+import infoImg from '../assets/img/fiverr-show.webp'
 
 export function HomePage() {
     const { companyImages, categoryIcons, categoryTexts } = galleryService
@@ -17,28 +19,47 @@ export function HomePage() {
                     ))}
                 </div>
             </section>
-            <section className='notes-section'>
-                <h2>The best part? Everything.</h2>
-                <ul>
-                    <li>
-                        <h3>Stick to your budget</h3>
-                        <p>Find the right service for every price point. No hourly rates, just project-based pricing.</p>
-                    </li>
-                    <li>
-                        <h3>Get quality work done quickly</h3>
-                        <p>Hand your project over to a talented freelancer in minutes, get long-lasting results.</p>
-                    </li>
-                    <li>
-                        <h3>Pay when you're happy</h3>
-                        <p>Upfront quotes mean no surprises. Payments only get released when you approve.</p>
-                    </li>
-                    <li>
-                        <h3>Count on 24/7 support</h3>
-                        <p>Our round-the-clock support team is available to help anytime, anywhere.</p>
-                    </li>
-                </ul>
-            </section>
+
             <ServicesCarousel />
+
+            <div className='home-info-wrapper'>
+                <section className='info-section flex row'>
+                    <section className='text'>
+                        <h2>The best part? Everything.</h2>
+                        <ul>
+                            <li>
+                                <div className='flex row'>
+                                    <img src={customCheckmarkImg} />
+                                    <h3>Stick to your budget</h3>
+                                </div>
+                                <p>Find the right service for every price point. No hourly rates, just project-based pricing.</p>
+                            </li>
+                            <li>
+                                <div className='flex row'>
+                                    <img src={customCheckmarkImg} />
+                                    <h3>Get quality work done quickly</h3>
+                                </div>
+                                <p>Hand your project over to a talented freelancer in minutes, get long-lasting results.</p>
+                            </li>
+                            <li>
+                                <div className='flex row'>
+                                    <img src={customCheckmarkImg} />
+                                    <h3>Pay when you're happy</h3>
+                                </div>
+                                <p>Upfront quotes mean no surprises. Payments only get released when you approve.</p>
+                            </li>
+                            <li>
+                                <div className='flex row'>
+                                    <img src={customCheckmarkImg} />
+                                    <h3>Count on 24/7 support</h3>
+                                </div>
+                                <p>Our round-the-clock support team is available to help anytime, anywhere.</p>
+                            </li>
+                        </ul>
+                    </section>
+                    <img src={infoImg} />
+                </section>
+            </div>
             <section className='home-categories-section'>
                 <h2>You need it, we've got it</h2>
                 <div className='categories grid'>
