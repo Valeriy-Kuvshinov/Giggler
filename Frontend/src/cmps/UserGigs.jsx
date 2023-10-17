6864b39a7e3b4f46676368e8a97ec38590b5b8ca
RoeiTal123 <roeit1977@gmail.com>
2023-10-17 15:14:04 +0300

    now gigs in profile in grid

--- Frontend/src/cmps/UserGigs.jsx
+++ Frontend/src/cmps/UserGigs.jsx
@@ -2,10 +2,14 @@ import { Link } from 'react-router-dom'
 
 import { GigPreview } from './GigPreview.jsx'
 
+import seller from '../assets/img/svg/become.seller.icon.svg'
+
 export function UserGigs({ user, gigs }) {
     const is = 'userProfile'
 
-    if (!gigs || gigs.length === 0) return <div>No gigs yet, maybe create one ^_^</div>
+    // gigs=[]
+
+    if (!gigs) return <div>No gigs yet, maybe create one ^_^</div>
 
     const userGigs = gigs.filter(gig => gig.ownerId === user._id)
 
@@ -14,18 +18,33 @@ export function UserGigs({ user, gigs }) {
             <div className='info-block title'>
                 Active Gigs
             </div>
-            <div className='info-block gig'>
+
+            {(gigs.length===0) && <div className='info-block gig seller'>
+                <Link to="/gig/edit" className="gig-creation-btn">
+                    <img src={seller}/>
+                    {/* <button>+</button> */}
+                    <span>Become A Seller</span>
+                </Link>
+            </div>
+            }
+            <div className='the-gigs'>
+
+
+            {(gigs.length!==0) && <div className='info-block gig'>
                 <Link to="/gig/edit" className="gig-creation-btn">
                     <button>+</button>
                     <span>Create a new Gig</span>
                 </Link>
-            </div>
+            </div>}
 
-            {userGigs.map(gig => (
+            {(gigs.length!==0) &&  (userGigs.map(gig => (
                 <div className='info-block' key={gig._id}>
                     <GigPreview is={is} gig={gig} />
                 </div>
-            ))}
+            )))}
+
+            </div>
+            
         </section>
     )
 }
