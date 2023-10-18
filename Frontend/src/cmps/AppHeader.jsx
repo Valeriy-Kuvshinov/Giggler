3566aafc56bc7da227701f3a1f7afb5b0afec0b1
TAC42 <tomi.carlin@gmail.com>
2023-10-18 18:44:15 -0400

    Fixed the filter! and corrected all the mistakes that required a refresh to be used to see the change! also fixed query params so it only renders from the filter in the store!

--- Frontend/src/cmps/AppHeader.jsx
+++ Frontend/src/cmps/AppHeader.jsx
@@ -18,31 +18,34 @@ export function AppHeader() {
     const navigate = useNavigate()
     const user = useSelector(storeState => storeState.userModule.user)
     const filterBy = useSelector((storeState) => storeState.gigModule.filterBy)
-
     const { showModal, openLogin, openSignup } = useModal()
     const categories = category
     const isHomePage = location.pathname === '/'
 
-  useEffect(() => {
-    const handleScroll = () => {
-      if (isHomePage) {
-        if (window.scrollY < 50) setHeaderStage(0)
-        else if (window.scrollY < 150) setHeaderStage(1)
-        else setHeaderStage(2)
-    }, [isHomePage])
-
-    async function onLogout() {
+    useEffect(() => {
+      const handleScroll = () => {
+        if (isHomePage) {
+          if (window.scrollY < 50) setHeaderStage(0)
+          else if (window.scrollY < 150) setHeaderStage(1)
+          else setHeaderStage(2)
+        }
+      }
+    
+      async function onLogout() {
         try {
-            navigate("/")
-            await logout()
+          navigate("/")
+          await logout()
+        } catch (err) {
+          showErrorMsg('Cannot logout')
         }
-        catch (err) {
-            showErrorMsg('Cannot logout')
-        }
-    }
-    window.addEventListener('scroll', handleScroll)
-    return () => window.removeEventListener('scroll', handleScroll)
-  }, [isHomePage])
+      }
+    
+      window.addEventListener('scroll', handleScroll)
+      return () => {
+        window.removeEventListener('scroll', handleScroll)
+      }
+    }, [isHomePage])
+    
 
   useEffect(() => {
     if (isHomePage) setHeaderStage(0)
@@ -64,59 +67,120 @@ export function AppHeader() {
     }
     window.addEventListener('click', closeDropdown)
 
-    function toggleDropdown(e) {
-        e.stopPropagation()
-        setShowDropdown(!showDropdown)
-    }
+    // function toggleDropdown(e) {
+    //     e.stopPropagation()
+    //     setShowDropdown(!showDropdown)
+    // }
   }, [])
 
   function handleSearchChange(e) {
     const newSearchQuery = e.target.value
     setSearchQuery(newSearchQuery)
   }
+
   function handleSearchSubmit(e) {
     e.preventDefault()
     if (!searchQuery) return
     setFilter({ ...filterBy, search: searchQuery })
     navigate(`/explore`)
-}
+  }
 
-    return (
-        <header className={`app-header flex column full ${isHomePage ? 'home-page' : ''} ${showModal ? 'show-modal' : ''}`} style={{ backgroundColor: headerBgColor }}>
-            <nav className="main-nav" style={{ borderBottom: mainNavBorder }}>
-                <div className="container flex row" style={{ color: textColor }}>
-                    <Link to="/" style={{ color: textColor }}>
-                        <h1 className="logo">Giggler<span>.</span></h1>
-                    </Link>
-                    <SearchBar
-                        placeholder="Search for any service..."
-                        searchQuery={searchQuery}
-                        onSearchChange={handleSearchChange}
-                        onSearchSubmit={handleSearchSubmit}
-                        visibility={searchVisibility}
-                    />
-                    <ul className="nav-links flex">
-                        <li><Link to="/explore" style={{ color: textColor }}>Explore</Link></li>
-                        {user ? (
-                            <>
-                                <li className="user-info flex" onClick={toggleDropdown}>
-                                    {user.imgUrl && <img src={user.imgUrl} />}
-                                    {showDropdown && <UserDropdown user={user} onClose={toggleDropdown} />}
-                                </li>
-                                <li>
-                                    <button className='logout' onClick={onLogout} style={{ color: textColor }}>Logout</button>
-                                </li>
-                            </>
-                        ) : (
-                            <>
-                                <li><button className='login' onClick={openLogin} style={{ color: textColor }}>Sign In</button></li>
-                                <li><button className='join' onClick={openSignup} style={{ color: joinBtnColor, borderColor: joinBtnColor }}>Join</button></li>
-                            </>
-                        )}
-                    </ul>
-                </div>
-            </nav>
-            <NavBar categories={categories} display={categoryNavDisplay} headerStage={headerStage} />
-        </header>
-    )
+  function setCatFilter(category) {
+    setFilter({ ...filterBy, cat: category })
+  }
+
+  const toggleDropdown = (e) => {
+    e.stopPropagation()
+    setShowDropdown(!showDropdown)
+  }
+
+  const headerBgColor = headerStage >= 1 ? '#fff' : 'transparent'
+  const searchVisibility = headerStage === 2 ? 'visible' : 'hidden'
+  const mainNavBorder = headerStage === 2 ? '1px solid #ced1d6' : 'none'
+  const categoryNavDisplay = headerStage === 2 ? 'flex' : 'none'
+  const textColor = isHomePage && headerStage === 0 ? '#fff' : '#62646a'
+  const joinBtnColor = headerStage >= 1 || !isHomePage ? '#1dbf73' : '#fff'
+
+  return (
+    <header
+      className={`app-header flex column full ${
+        isHomePage ? 'home-page' : ''
+      } ${showModal ? 'show-modal' : ''}`}
+      style={{ backgroundColor: headerBgColor }}
+    >
+      <nav className="main-nav" style={{ borderBottom: mainNavBorder }}>
+        <div className="container flex row" style={{ color: textColor }}>
+          <Link to="/" style={{ color: textColor }}>
+            <h1 className="logo">
+              Giggler<span>.</span>
+            </h1>
+          </Link>
+
+          <SearchBar
+            placeholder="Search for any service..."
+            searchQuery={searchQuery}
+            onSearchChange={handleSearchChange}
+            onSearchSubmit={handleSearchSubmit}
+            visibility={searchVisibility}
+          />
+          <ul className="nav-links flex">
+            <li>
+              <Link to="/explore" style={{ color: textColor }}>
+                Explore
+              </Link>
+            </li>
+            {user ? (
+              <>
+                <li className="user-info flex" onClick={toggleDropdown}>
+                  {user.imgUrl && <img src={user.imgUrl} />}
+                  {showDropdown && (
+                    <UserDropdown user={user} onClose={toggleDropdown} />
+                  )}
+                </li>
+                <li>
+                  <button
+                    className="logout"
+                    onClick={onLogout}
+                    style={{ color: textColor }}
+                  >
+                    Logout
+                  </button>
+                </li>
+              </>
+            ) : (
+              <>
+                <li>
+                  <button
+                    className="login"
+                    onClick={() => setShowModal('login')}
+                    style={{ color: textColor }}
+                  >
+                    Sign In
+                  </button>
+                </li>
+                <li>
+                  <button
+                    className="join"
+                    onClick={() => setShowModal('signup')}
+                    style={{ color: joinBtnColor, borderColor: joinBtnColor }}
+                  >
+                    Join
+                  </button>
+                </li>
+              </>
+            )}
+          </ul>
+        </div>
+      </nav>
+      <NavBar
+        categories={categories}
+        display={categoryNavDisplay}
+        headerStage={headerStage}
+        setCatFilter={setCatFilter}
+      />
+      {showModal && (
+        <LoginSignup mode={showModal} closeModal={() => setShowModal('')} />
+      )}
+    </header>
+  )
 }
