cac456db0ae67ea120b10be61be7b1e16e2ae95e
Valeriy-Kuvshinov <valerykuvshinuv@gmail.com>
2023-10-19 16:01:00 +0300

    liking the gig now gets rendered in real time, liking a gig without logging in will show the login modal

--- Frontend/src/cmps/GigNavbar.jsx
+++ Frontend/src/cmps/GigNavbar.jsx
@@ -2,52 +2,72 @@ import heart from "../assets/img/svg/heart.icon.svg"
 import likedHeart from "../assets/img/svg/liked.heart.icon.svg"
 import share from "../assets/img/svg/share.icon.svg"
 
+import { useState, useEffect } from "react"
 import { useSelector } from 'react-redux'
+import { useModal } from "../customHooks/ModalContext"
 
-import { CatTagDisplayBar } from "./CatTagDisplayBar"
+import { CatTagDisplayBar } from "./CatTagDisplayBar.jsx"
 
 import { gigService } from "../services/gig.service"
 
-export function GigNavbar({ gig }) {
-  
+
+export function GigNavbar({ gig, onGigChange }) {
   const user = useSelector(storeState => storeState.userModule.user)
-  var isLiked=false
-  if(user!==null){
-    isLiked=((gig.likedByUsers.findIndex(liked => liked===user._id))!==-1) ? true : false
-  }
-  // console.log('isLiked ',isLiked)
-  // console.log('loggedin ',user)
+  const { openLogin } = useModal()
+
+  const [isLiked, setIsLiked] = useState(
+    user && gig.likedByUsers.includes(user._id)
+  )
+
+  useEffect(() => {
+    setIsLiked(user && gig.likedByUsers.includes(user._id))
+  }, [user, gig])
 
   function shareGig() {
     console.log("share")
   }
 
   function likeGig() {
-    if(user===null) {
-      alert('log in to like gig')
+    if (!user) {
+      openLogin()
       return
     }
-    const gigToSave={...gig}
-    if(gigToSave.likedByUsers.findIndex(gig=>gig===user._id)!==-1){
-      var likedByUsers=[...gigToSave.likedByUsers]
-      likedByUsers=likedByUsers.filter(liker => liker!== user._id)
-      gigToSave.likedByUsers=[...likedByUsers]
+    const gigToSave = { ...gig }
+
+    if (gigToSave.likedByUsers.includes(user._id)) {
+      gigToSave.likedByUsers = gigToSave.likedByUsers.filter(
+        liker => liker !== user._id
+      )
+      setIsLiked(false)
+
       gigService.save(gigToSave)
-      console.log("unlike")
-    } else {
+        .then(() => {
+          onGigChange(gigToSave)
+        })
+    }
+    else {
       gigToSave.likedByUsers.push(user._id)
+
+      setIsLiked(true)
+
       gigService.save(gigToSave)
-      console.log("like")
+        .then(() => {
+          onGigChange(gigToSave)
+        })
     }
   }
+
   return (
     <div className="gig-navbar">
       <CatTagDisplayBar category={gig.category} tag={gig.tags[1]} />
+
       <div className="gig-interactions">
         <button onClick={likeGig}>
-          <img src={(isLiked)?likedHeart:heart} title="liked the gig"/>
+          <img src={isLiked ? likedHeart : heart} title="liked the gig" />
         </button>
+
         <span>{gig.likedByUsers.length}</span>
+
         <button onClick={shareGig} className="share" title="share the gig">
           <img src={share} />
         </button>
