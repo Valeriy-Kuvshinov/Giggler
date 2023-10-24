ef1c3f158407406db0a76069dd4c594d6155af5e
Valeriy-Kuvshinov <valerykuvshinuv@gmail.com>
2023-10-24 12:01:44 +0300

    initial new dashboard reorganization

--- Frontend/src/cmps/UserOrders.jsx
+++ Frontend/src/cmps/UserOrders.jsx
@@ -1,18 +1,18 @@
 import { useEffect } from 'react'
 import { useSelector } from 'react-redux'
 
-import { UserOrder } from './UserOrder'
+import { UserOrder } from './UserOrder.jsx'
 
-import { loadOrders } from '../store/order.actions'
+import { loadOrders } from '../store/order.actions.js'
 
-import { orderBackendService } from '../services/order.backend.service'
-import { userService } from '../services/user.service'
+import { orderBackendService } from '../services/order.backend.service.js'
+import { userService } from '../services/user.service.js'
 
 export function UserOrders() {
     const user = useSelector(storeState => storeState.userModule.user)
     const orders = useSelector(storeState => storeState.orderModule.orders)
-    var sellerOrders=orders.filter((order)=>order.sellerId===user._id)
-    var userOrders=orders.filter((order)=>(order.buyerId===user._id)&&(order.orderState!=='pending'))
+    var sellerOrders = orders.filter((order) => order.sellerId === user._id)
+    var userOrders = orders.filter((order) => (order.buyerId === user._id) && (order.orderState !== 'pending'))
 
     useEffect(() => {
         loadTheOrders()
@@ -26,52 +26,52 @@ export function UserOrders() {
         }
     }
 
-    // console.log('orders from backend : ',orders)
-
     function acceptOrder(order) {
         console.log(`order ${order._id} accepted`)
         order.orderState = 'accepted'
-        order.acceptedAt=Date.now()
-        var updatedUser={...user}
-        updatedUser.lastDelivery=Date.now()
+        order.acceptedAt = Date.now()
+        var updatedUser = { ...user }
+        updatedUser.lastDelivery = Date.now()
         userService.update(updatedUser)
         orderBackendService.save(order)
     }
 
-    function denyOrder(order,reason) {
+    function denyOrder(order, reason) {
         console.log(`order ${order._id} denied`)
-        const newOrder={...order }
+        const newOrder = { ...order }
         newOrder.orderState = 'denied'
-        newOrder.deniedAt=Date.now()
-        var updatedUser={...user}
-        updatedUser.lastDelivery=Date.now()
+        newOrder.deniedAt = Date.now()
+        var updatedUser = { ...user }
+        updatedUser.lastDelivery = Date.now()
         userService.update(updatedUser)
         newOrder.reasonForDenial = reason
         orderBackendService.save(newOrder)
     }
 
-    if(orders.length===0) return <div>loading... </div>
+    if (orders.length === 0) return <div>loading... </div>
 
-    return (<section className="user-orders">
-        <ul className='orders'>
-            <div className='orders-title'>
-                Active Orders
-            </div>
-            {sellerOrders.map((order) =>
-                <li key={order._id}>
-                    <UserOrder order={order} acceptOrder={acceptOrder} denyOrder={denyOrder} />
-                </li>
-            )}
-        </ul>
-        <ul className='orders'>
-            <div className='orders-title'>
-                Recieved Orders
-            </div>
-            {userOrders.map((order) =>
-                <li key={order._id}>
-                    <UserOrder order={order} acceptOrder={acceptOrder} denyOrder={denyOrder} />
-                </li>
-            )}
-        </ul>
-    </section>)
+    return (
+        <section className="user-orders">
+            <ul className='orders'>
+                <div className='orders-title'>
+                    Active Orders
+                </div>
+                {sellerOrders.map((order) =>
+                    <li key={order._id}>
+                        <UserOrder order={order} acceptOrder={acceptOrder} denyOrder={denyOrder} />
+                    </li>
+                )}
+            </ul>
+            <ul className='orders'>
+                <div className='orders-title'>
+                    Recieved Orders
+                </div>
+                {userOrders.map((order) =>
+                    <li key={order._id}>
+                        <UserOrder order={order} acceptOrder={acceptOrder} denyOrder={denyOrder} />
+                    </li>
+                )}
+            </ul>
+        </section>
+    )
 }
