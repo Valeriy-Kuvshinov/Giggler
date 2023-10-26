848ce9b9beb0f9e98087141e958f86275f4dc84c
Valeriy-Kuvshinov <valerykuvshinuv@gmail.com>
2023-10-26 11:58:36 +0300

    dropdown added

--- Frontend/src/services/svg.service.js
+++ Frontend/src/services/svg.service.js
@@ -5,7 +5,7 @@ const ICONS = {
   star: `<svg width="16" height="15" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M16 5.81285C16 5.98299 15.875 6.14367 15.75 6.26654L12.2596 9.61248L13.0865 14.3384C13.0962 14.4045 13.0962 14.4612 13.0962 14.5274C13.0962 14.7732 12.9808 15 12.7019 15C12.5673 15 12.4327 14.9527 12.3173 14.8866L8 12.656L3.68269 14.8866C3.55769 14.9527 3.43269 15 3.29808 15C3.01923 15 2.89423 14.7732 2.89423 14.5274C2.89423 14.4612 2.90385 14.4045 2.91346 14.3384L3.74038 9.61248L0.240385 6.26654C0.125 6.14367 0 5.98299 0 5.81285C0 5.5293 0.298077 5.41588 0.538462 5.37807L5.36539 4.68809L7.52885 0.387524C7.61539 0.207939 7.77885 0 8 0C8.22115 0 8.38462 0.207939 8.47115 0.387524L10.6346 4.68809L15.4615 5.37807C15.6923 5.41588 16 5.5293 16 5.81285Z"></path></svg>`,
 
   heart: `<svg width="34" height="32" viewBox="0 0 34 32" fill="none" xmlns="http://www.w3.org/2000/svg">
-  <g clip-path="url(#clip0_4_20)">
+    <g clip-path="url(#clip0_4_20)">
       <g filter="url(#filter0_d_4_20)">
           <path
               d="M5 11.333c0 2.193.926 4.223 2.399 5.515L17 26l9.55-9.155A7.374 7.374 0 0029 11.333C29 7.283 25.776 4 21.8 4A7.1 7.1 0 0017 5.867 7.1 7.1 0 0012.2 4C8.224 4 5 7.283 5 11.333z"
@@ -16,8 +16,8 @@ const ICONS = {
               d="M7.916 16.305l-.011-.01-.012-.01C6.592 15.141 5.75 13.32 5.75 11.332c0-3.649 2.9-6.583 6.45-6.583a6.35 6.35 0 014.295 1.672l.505.46.505-.46A6.35 6.35 0 0121.8 4.75c3.55 0 6.45 2.934 6.45 6.583a6.624 6.624 0 01-2.2 4.953l-.01.009-.01.009-9.032 8.658-9.082-8.657z"
               stroke="#fff" stroke-width="1.5" />
       </g>
-  </g>
-  <defs>
+    </g>
+    <defs>
       <filter id="filter0_d_4_20" x="0" y="0" width="34" height="32" filterUnits="userSpaceOnUse"
           color-interpolation-filters="sRGB">
           <feFlood flood-opacity="0" result="BackgroundImageFix" />
@@ -38,11 +38,11 @@ const ICONS = {
           <feBlend in2="BackgroundImageFix" result="effect1_dropShadow_4_20" />
           <feBlend in="SourceGraphic" in2="effect1_dropShadow_4_20" result="shape" />
       </filter>
-  </defs>
-</svg>`,
+    </defs>
+  </svg>`,
 
   heartLiked: `<svg width="34" height="32" viewBox="0 0 34 32" fill="none" xmlns="http://www.w3.org/2000/svg">
-<g clip-path="url(#clip0_4_20)">
+  <g clip-path="url(#clip0_4_20)">
     <g filter="url(#filter0_d_4_20)">
         <path
             d="M5 11.333c0 2.193.926 4.223 2.399 5.515L17 26l9.55-9.155A7.374 7.374 0 0029 11.333C29 7.283 25.776 4 21.8 4A7.1 7.1 0 0017 5.867 7.1 7.1 0 0012.2 4C8.224 4 5 7.283 5 11.333z"
@@ -53,8 +53,8 @@ const ICONS = {
             d="M7.916 16.305l-.011-.01-.012-.01C6.592 15.141 5.75 13.32 5.75 11.332c0-3.649 2.9-6.583 6.45-6.583a6.35 6.35 0 014.295 1.672l.505.46.505-.46A6.35 6.35 0 0121.8 4.75c3.55 0 6.45 2.934 6.45 6.583a6.624 6.624 0 01-2.2 4.953l-.01.009-.01.009-9.032 8.658-9.082-8.657z"
             stroke="#fff" stroke-width="1.5" />
     </g>
-</g>
-<defs>
+  </g>
+  <defs>
     <filter id="filter0_d_4_20" x="0" y="0" width="34" height="32" filterUnits="userSpaceOnUse"
         color-interpolation-filters="sRGB">
         <feFlood flood-opacity="0" result="BackgroundImageFix" />
@@ -87,6 +87,18 @@ const ICONS = {
   pageArrowLeft: `<svg width="16" height="16" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" fill="#74767E"><path d="M5.093 13.031c.285.3.747.3 1.031 0a.801.801 0 0 0 0-1.09l-2.803-2.96h10.21c.402 0 .728-.345.728-.77 0-.426-.326-.77-.729-.77H3.173l2.951-3.118a.801.801 0 0 0 0-1.09.702.702 0 0 0-1.031 0L.97 7.589a.801.801 0 0 0 0 1.09l.07.072a.744.744 0 0 0 .01.01l4.043 4.271Z"></path></svg>`,
 
   pageArrowRight: `<svg width="16" height="16" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" fill="#74767E"><path d="M9.923 2.969a.702.702 0 0 0-1.031 0 .801.801 0 0 0 0 1.09l2.804 2.96H1.486c-.403 0-.73.345-.73.77 0 .426.327.77.73.77h10.358l-2.952 3.118a.801.801 0 0 0 0 1.09c.285.3.747.3 1.031 0l4.123-4.355a.801.801 0 0 0 0-1.09l-.069-.072a1.422 1.422 0 0 0-.01-.01L9.923 2.969Z"></path></svg>`,
+
+  headerDropdownGray: `<svg xmlns="http://www.w3.org/2000/svg" width="23" height="19" viewBox="0 0 23 19">
+  <rect y="16" width="23" height="3" rx="1.5" fill="#555" />
+  <rect width="23" height="3" rx="1.5" fill="#555" />
+  <rect y="8" width="23" height="3" rx="1.5" fill="#555" />
+  </svg>`,
+
+  headerDropdownWhite: `<svg xmlns="http://www.w3.org/2000/svg" width="23" height="19" viewBox="0 0 23 19">
+  <rect y="16" width="23" height="3" rx="1.5" fill="#62646a" />
+  <rect width="23" height="3" rx="1.5" fill="#62646a" />
+  <rect y="8" width="23" height="3" rx="1.5" fill="#62646a" />
+  </svg>`,
 }
 
 function getIcon(svgName) {
