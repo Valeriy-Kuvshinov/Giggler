ff0d48e522383efaadb3c550aa6793c8e54574cc
TAC42 <tomi.carlin@gmail.com>
2023-10-29 18:56:13 -0400

    fixed the radio buttons in purchase page plus other stuff.

--- Frontend/src/cmps/PaymentDetails.jsx
+++ Frontend/src/cmps/PaymentDetails.jsx
@@ -1,17 +1,17 @@
-import SvgIcon from "./SvgIcon.jsx"
+import SvgIcon from './SvgIcon.jsx'
 
-import { useState } from "react"
-import { useForm } from "../customHooks/useForm.js"
+import { useState } from 'react'
+import { useForm } from '../customHooks/useForm.js'
 
 export function PaymentDetails() {
   const [paymentMethod, setPaymentMethod] = useState(true)
 
   const initialState = {
-    crdNum: "1111222233334444",
-    expDate: new Date().getMonth() + 1 + "/" + (new Date().getFullYear() % 100),
-    pinCode: "123",
-    firstName: "yaron",
-    lastName: "biton",
+    crdNum: '1111222233334444',
+    expDate: new Date().getMonth() + 1 + '/' + (new Date().getFullYear() % 100),
+    pinCode: '123',
+    firstName: 'Yaron',
+    lastName: 'Biton',
   }
 
   const [fields, , handleChange] = useForm(initialState)
@@ -19,86 +19,105 @@ export function PaymentDetails() {
 
   return (
     <section className="payment-details">
-      <section className="details one">
+      <section className="details section-header">
         <span>Payment Options</span>
       </section>
+      <section className="payment-methods">
+        <article
+          className="details credit-card"
+          onClick={() => setPaymentMethod(true)}
+        >
+          <label className={`credit-type ${paymentMethod ? 'selected' : ''}`}>
+            <input type="radio" className="credit" />
+            {/* checked={paymentMethod} */}
+            <span className="radio-btn"></span>
+            <span className="text">Credit & Debit Cards</span>
+            <SvgIcon iconName={'creditCardsIcon'} />
+          </label>
 
-      <section className="details two" onClick={() => setPaymentMethod(true)}>
-        <div className="credit-type">
-          <input type="radio" className="credit" />
-          <span className="radio-btn"></span>
-          <span className="text">Credit & Debit Cards</span>
-          <SvgIcon iconName={'creditCardsIcon'} />
-        </div>
-      </section>
+          <div
+            className={`details credit-info ${paymentMethod ? '' : 'hidden'}`}
+          >
+            <div className="credit-details">
+              <div className="credit-number">
+                <span>Card Number</span>
+                <div className="card-number-input">
+                  <span
+                    className="card-logo"
+                    style={{
+                      backgroundImage:
+                        'url("https://fiverr-res.cloudinary.com/image/upload/f_png,q_auto/v1/attachments/generic_asset/asset/2496be9dfb5983d9de91630d83bb21e0-1682945799754/generic.svg")',
+                    }}
+                  ></span>
 
-      <section className={`details three ${paymentMethod ? '' : 'hidden'}`}>
-        <div className="credit-details">
-          <div className="credit-number">
-            <span>Card Number</span>
-            <input
-              type="text"
-              name="crdNum"
-              value={crdNum}
-              onChange={handleChange}
-              placeholder="0000 0000 0000 0000"
-            />
+                  <input
+                    type="text"
+                    name="crdNum"
+                    value={crdNum}
+                    onChange={handleChange}
+                    placeholder="0000 0000 0000 0000"
+                  />
+                </div>
+              </div>
+
+              <div className="private">
+                <div className="credit-date">
+                  <span>Expiration Date</span>
+                  <input
+                    type="text"
+                    name="expDate"
+                    value={expDate}
+                    onChange={handleChange}
+                    placeholder="MM/YY"
+                  />
+                </div>
+
+                <div className="credit-date">
+                  <span>Security Code</span>
+                  <input
+                    type="text"
+                    name="pinCode"
+                    value={pinCode}
+                    onChange={handleChange}
+                    placeholder="XXX"
+                  />
+                </div>
+              </div>
+
+              <div className="private">
+                <div className="buyer-info">
+                  <span>First Name</span>
+                  <input
+                    type="text"
+                    name="firstName"
+                    value={firstName}
+                    onChange={handleChange}
+                  />
+                </div>
+
+                <div className="buyer-info">
+                  <span>Last Name</span>
+                  <input
+                    type="text"
+                    name="lastName"
+                    value={lastName}
+                    onChange={handleChange}
+                  />
+                </div>
+              </div>
+            </div>
           </div>
-
-          <div className="private">
-            <div className="credit-date">
-              <span>Expiration Date</span>
-              <input
-                type="text"
-                name="expDate"
-                value={expDate}
-                onChange={handleChange}
-                placeholder="MM/YY"
-              />
-            </div>
-
-            <div className="credit-date">
-              <span>Security Code</span>
-              <input
-                type="text"
-                name="pinCode"
-                value={pinCode}
-                onChange={handleChange}
-                placeholder="XXX"
-              />
-            </div>
-          </div>
-
-          <div className="private">
-            <div className="buyer-info">
-              <span>First Name</span>
-              <input
-                type="text"
-                name="firstName"
-                value={firstName}
-                onChange={handleChange}
-              />
-            </div>
-
-            <div className="buyer-info">
-              <span>Last Name</span>
-              <input
-                type="text"
-                name="lastName"
-                value={lastName}
-                onChange={handleChange}
-              />
-            </div>
-          </div>
-        </div>
-      </section>
-
-      <section className="details four" onClick={() => setPaymentMethod(false)}>
-        <div className="credit-type">
-          <input type="radio" className="credit" />
-          <span className="radio-btn"></span>
-          <SvgIcon iconName={'paypalIcon'} />
-        </div>
+        </article>
+        <article
+          className="details paypal"
+          onClick={() => setPaymentMethod(false)}
+        >
+          <label className={`credit-type ${!paymentMethod ? 'selected' : ''}`}>
+            <input type="radio" className="credit" checked={paymentMethod} />
+            <span className="radio-btn"></span>
+            <SvgIcon iconName={'paypalIcon'} />
+          </label>
+        </article>
       </section>
     </section>
   )
