caa31e966f06d5fa65f12b30d7ba4a3f6eb46ee2
TAC42 <tomi.carlin@gmail.com>
2023-10-28 20:06:17 -0400

    finalizing the gig-details-aside-package

--- Frontend/src/cmps/GigDetailsAside.jsx
+++ Frontend/src/cmps/GigDetailsAside.jsx
@@ -1,4 +1,3 @@
-import clock from '../assets/img/svg/clock.icon.svg'
 import refresh from '../assets/img/svg/refresh.icon.svg'
 import checkmark from '../assets/img/svg/checkmark.icon.svg'
 import arrow from '../assets/img/svg/arrow.icon.svg'
@@ -20,10 +19,60 @@ export function GigDetailsAside({ gig, onGigChange }) {
 
   const { openLogin } = useModal()
   const [isModalOpen, setIsModalOpen] = useState(false)
-  const [SelectedPackage ,setSelectedPackage] = useState('basic')
+  const [SelectedPackage, setSelectedPackage] = useState('basic')
   const [isLiked, setIsLiked] = useState(
     user && gig.likedByUsers.includes(user._id)
   )
+  const packages = {
+    basic: {
+      type: 'Starter Package',
+      price: 1 * gig.price,
+      desc: `2 logo concepts, jpg, transparent png`,
+      time: gig.daysToMake,
+      revisions: '8',
+      features: [
+        '2 concepts included',
+        'Logo transparency',
+        'Vector file',
+        'Printable file',
+        'Include 3D mockup',
+        'Include source file',
+      ],
+      featuresCond: [true, true, false, true, true, false],
+    },
+    standard: {
+      type: 'Standard Package',
+      price: 3 * gig.price,
+      desc: `2 logo concepts + jpg file, transparent png, source files + 3D Mockup`,
+      time: gig.daysToMake,
+      revisions: 'Unlimited',
+      features: [
+        '2 concepts included',
+        'Logo transparency',
+        'Vector file',
+        'Printable file',
+        'Include 3D mockup',
+        'Include source file',
+      ],
+      featuresCond: [true, true, false, true, true, true],
+    },
+    premium: {
+      type: 'Pro Package',
+      price: 5 * gig.price,
+      desc: `3 logo concepts+ jpg, png+ all source & vector files + 3D Mockup`,
+      time: gig.daysToMake,
+      revisions: 'Unlimited',
+      features: [
+        '3 concepts included',
+        'Logo transparency',
+        'Vector file',
+        'Printable file',
+        'Include 3D mockup',
+        'Include source file',
+      ],
+      featuresCond: [true, true, true, true, true, true],
+    },
+  }
 
   useEffect(() => {
     setIsLiked(user && gig.likedByUsers.includes(user._id))
@@ -92,74 +141,74 @@ export function GigDetailsAside({ gig, onGigChange }) {
 
       {isModalOpen && <ShareGigModal onClose={closeModal} />}
 
-      <section className="gig-order">
-        <div className="package-tabs">
-          <button className={`package tab-1 ${SelectedPackage === 'basic' ? 'checked' : ''}`} onClick={()=>setSelectedPackage('basic')}>Basic</button>
-          <button className={`package tab-2 ${SelectedPackage === 'standard' ? 'checked' : ''}`} onClick={()=>setSelectedPackage('standard')}>Standard</button>
-          <button className={`package tab-3 ${SelectedPackage === 'premium' ? 'checked' : ''}`} onClick={()=>setSelectedPackage('premium')}>Premium</button>
+      <div className="package-tabs">
+        <button
+          className={`btn-package tab-1 ${
+            SelectedPackage === 'basic' ? 'checked' : ''
+          }`}
+          onClick={() => setSelectedPackage('basic')}
+        >
+          Basic
+        </button>
+        <button
+          className={`btn-package tab-2 ${
+            SelectedPackage === 'standard' ? 'checked' : ''
+          }`}
+          onClick={() => setSelectedPackage('standard')}
+        >
+          Standard
+        </button>
+        <button
+          className={`btn-package tab-3 ${
+            SelectedPackage === 'premium' ? 'checked' : ''
+          }`}
+          onClick={() => setSelectedPackage('premium')}
+        >
+          Premium
+        </button>
+      </div>
+
+      <section className="package-content">
+        <div className="type-price">
+          <span className="type">{packages[SelectedPackage].type}</span>
+          <span className="price">${packages[SelectedPackage].price}</span>
         </div>
-
-
-
-        <div className="title">
-          <span>Order Details</span>
-          <span>${gig.price}</span>
-        </div>
-        <p>
-          1 custom logo + high resolution file + 3d mockup + logo transparency +
-          300dpi
-        </p>
-        <div className="shipping-info">
-          <div className="inside-shipping">
-            <img src={clock} />
-            <span> {gig.daysToMake} Delivery</span>
+        <p>{packages[SelectedPackage].desc}</p>
+        <div className="additional-info">
+          <div className="delivery-wrapper">
+            <SvgIcon iconName={'clock'} />
+            <span className="delivery b">
+              {' '}
+              {packages[SelectedPackage].time} Delivery
+            </span>
           </div>
-
-          <div className="inside-shipping">
-            <img src={refresh} />
-            <span>Unlimited Revisions</span>
+          <div className="revisions-wrapper">
+            <SvgIcon iconName={'refresh'} />
+            <span className="revisions b">{`${packages[SelectedPackage].revisions} Revisions`}</span>
           </div>
         </div>
-        <ul>
-          <li>
-            <img src={checkmark} />1 concept included
-          </li>
-
-          <li>
-            <img src={checkmark} />
-            Logo transparency
-          </li>
-
-          <li>
-            <img src={checkmark} />
-            Vector file
-          </li>
-
-          <li>
-            <img src={checkmark} />
-            Printable file
-          </li>
-
-          <li>
-            <img src={checkmark} />
-            Include 3D mockup
-          </li>
-
-          <li>
-            <img src={checkmark} />
-            Include source file
-          </li>
-
-          <li>
-            <img src={checkmark} />
-            Include social media kit
-          </li>
+        <ul className="features">
+          {packages[SelectedPackage].features.map((feature, idx) => (
+            <li key={idx}>
+              <SvgIcon
+                iconName={`${
+                  packages[SelectedPackage].featuresCond[idx]
+                    ? 'checked'
+                    : 'unchecked'
+                }`}
+              />
+              {feature}
+            </li>
+          ))}
         </ul>
-
         <span className="btn-continue" onClick={onContinue}>
           <span>Continue</span> <SvgIcon iconName={'pageArrowRight'} />
         </span>
       </section>
+      <div className="contact-seller-wrapper">
+        <span className="contact-seller b">Contact me</span>
+      </div>
+
     </section>
   )
 }
