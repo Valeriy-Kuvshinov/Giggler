41be6edf6b0b2feea7f0b46765e42cdedec52c95
RoeiTal123 <roeit1977@gmail.com>
2023-11-02 19:34:25 +0200

    added more to description

--- Frontend/src/cmps/UserInfo.jsx
+++ Frontend/src/cmps/UserInfo.jsx
@@ -45,10 +45,10 @@ export function UserInfo({ user }) {
   let month = months[time.getMonth()]
   let year = time.getFullYear()
   let deliveredTime
-  if(!user.lastDeliveredAt){
-    deliveredTime=new Date(Date.now())
+  if (!user.lastDeliveredAt) {
+    deliveredTime = new Date(Date.now())
   } else {
-    deliveredTime=new Date(user.lastDeliveredAt)
+    deliveredTime = new Date(user.lastDeliveredAt)
   }
 
   function loadModal() {
@@ -90,35 +90,33 @@ export function UserInfo({ user }) {
     const isHalfStar = user.rating % 1 >= 0.5
 
     const stars = [...Array(fullStarsCount)].map((_, idx) => (
-      <SvgIcon iconName={'star'} key={utilService.makeId()}/>
+      <SvgIcon iconName={"star"} key={utilService.makeId()} />
     ))
 
     if (isHalfStar) {
-      stars.push(<SvgIcon iconName={'halfstar'} key={utilService.makeId()}/>)
+      stars.push(<SvgIcon iconName={"halfstar"} key={utilService.makeId()} />)
       fullStarsCount += 1
     }
 
     const emptyStarsCount = 5 - fullStarsCount
     for (let i = 0; i < emptyStarsCount; i++) {
-      stars.push(
-        <SvgIcon iconName={'emptystar'} key={utilService.makeId()}/>
-      )
+      stars.push(<SvgIcon iconName={"emptystar"} key={utilService.makeId()} />)
     }
     return stars
   }
 
-  let userLevel=''
-  if(user.level==='level 0') userLevel='newuser'
-  if(user.level==='level 1') userLevel='level1'
-  if(user.level==='level 2') userLevel='level2'
-  if(user.level==='level 3') userLevel='topuser'
+  let userLevel = ""
+  if (user.level === "level 0") userLevel = "newuser"
+  if (user.level === "level 1") userLevel = "level1"
+  if (user.level === "level 2") userLevel = "level2"
+  if (user.level === "level 3") userLevel = "topuser"
 
   return (
     <section className="user-info">
       <div className="info-block flex column">
         <div className="profile-picture">
-        <img src={user.imgUrl} onClick={loadModal} />
-        <SvgIcon iconName={userLevel}/>
+          <img src={user.imgUrl} onClick={loadModal} />
+          <SvgIcon iconName={userLevel} />
         </div>
 
         <h2>
@@ -140,7 +138,9 @@ export function UserInfo({ user }) {
         <div className="stars">
           {renderStars()}
           <span className="rating">{user.rating}</span>
-          <span className="review-count">({filteredReviews.length} reviews)</span>
+          <span className="review-count">
+            ({filteredReviews.length} reviews)
+          </span>
         </div>
 
         <div className="location-and-time">
@@ -168,7 +168,7 @@ export function UserInfo({ user }) {
               <span>Avg. Response Time</span>
             </span>
             <span className="bold">
-              {utilService.getRandomIntInclusive(2,12)} Hours
+              {utilService.getRandomIntInclusive(2, 12)} Hours
             </span>
           </div>
 
@@ -178,16 +178,17 @@ export function UserInfo({ user }) {
               <span>Last Delivery</span>
             </span>
             <span className="bold">
-              {months[deliveredTime.getMonth()].slice(0, 3)} {deliveredTime.getFullYear()}
+              {months[deliveredTime.getMonth()].slice(0, 3)}{" "}
+              {deliveredTime.getFullYear()}
             </span>
           </div>
         </div>
       </div>
 
-      <div className="info-block flex column">
-        <h3>Description</h3>
+      <div className="info-block flex column description">
+        <h3 className="description-title">Description</h3>
         {isEditing && loggedinUser._id === user._id ? (
-          <div>
+          <div className="description-box">
             <textarea
               className="description-area"
               value={description}
@@ -196,8 +197,43 @@ export function UserInfo({ user }) {
             />
           </div>
         ) : (
-          <p onClick={handleEditClick}>{description}</p>
+          <div className="description-box">
+            <p className="description-area" onClick={handleEditClick}>
+              {description}
+            </p>
+          </div>
         )}
+        <div className="languages">
+          <span className="title">Languages</span>
+          <div className="the-languages">
+            {user.languages.map((language) => (
+              <div className="language">
+                <span>{language.language}</span>
+                <span> - </span>
+                <span>{language.fluency}</span>
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className="skills">
+          <span className="title">Skills</span>
+          <div className="the-skills">
+            {user.skills.map(skill => 
+              <div className="skill">
+                <span>{skill}</span>
+              </div>)}
+          </div>
+        </div>
+        <div className="educations">
+          <span className="title">Education</span>
+          <div className="the-educations">
+            {user.education.map(education => 
+              <div className="education">
+                <span>{education.certificate}</span>
+                <span>{education.educationPlace}, Graduated {education.graduationTime}</span>
+              </div>)}
+          </div>
+        </div>
       </div>
 
       {isModal && <UserEditModal user={user} closeModal={closeModal} />}
