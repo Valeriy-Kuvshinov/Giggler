63468acf34a264d86a0a3408c56f0d270ceffff7
Valeriy-Kuvshinov <valerykuvshinuv@gmail.com>
2023-11-06 20:04:24 +0200

    orders are now shown in aside menu

--- Frontend/src/cmps/AsideMenu.jsx
+++ Frontend/src/cmps/AsideMenu.jsx
@@ -1,5 +1,9 @@
 import { Link, useNavigate } from 'react-router-dom'
+import { useState } from 'react'
 import { useModal } from '../customHooks/ModalContext.jsx'
+
+import { Accordion, AccordionSummary, AccordionDetails, Typography } from '@mui/material'
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
 
 import { BuyerOrders } from './BuyerOrders.jsx'
 
@@ -9,48 +13,62 @@ import { showErrorMsg } from '../services/event-bus.service.js'
 export function AsideMenu({ user, onClose }) {
     const navigate = useNavigate()
     const { openLogin, openSignup } = useModal()
+    const [expanded, setExpanded] = useState(false)
+
+    const handleChange = (panel) => (event, isExpanded) => {
+        setExpanded(isExpanded ? panel : false)
+    }
 
     async function onLogout() {
         try {
-            navigate('/')
             await logout()
+            navigate('/')
         } catch (err) {
             showErrorMsg('Cannot logout')
         }
     }
 
     return (
-        <div className="aside-menu flex column" onClick={onClose}>
+        <div className="aside-menu flex column">
             {user ? (
                 <>
-                    <Link to="/explore"> Explore </Link>
-                    <Link to="/">Become a Seller</Link>
-                    <Link to={`/user/${user._id}`}>Profile</Link>
-                    <Link to="/dashboard">Dashboard</Link>
-                    <button className="orders">Orders</button>
-                    <button className="logout"
-                        onClick={onLogout}>Logout</button>
+                    <Link to="/explore" onClick={onClose}> Explore </Link>
+                    <Link to="/" onClick={onClose}>Become a Seller</Link>
+                    <Link to={`/user/${user._id}`} onClick={onClose}>Profile</Link>
+                    <Link to="/dashboard" onClick={onClose}>Dashboard</Link>
+                    <Accordion expanded={expanded === 'panel1'} onChange={handleChange('panel1')}>
+                        <AccordionSummary
+                            expandIcon={<ExpandMoreIcon />}
+                            aria-controls="panel1bh-content"
+                            id="panel1bh-header"
+                            onClick={(e) => e.stopPropagation()}
+                        >
+                            <Typography>Orders</Typography>
+                        </AccordionSummary>
+                        <AccordionDetails
+                            onClick={(e) => e.stopPropagation()}
+                        >
+                            <BuyerOrders user={user} onClose={onClose} />
+                        </AccordionDetails>
+                    </Accordion>
+                    <button className="logout" onClick={onLogout}>Logout</button>
                 </>
             ) : (
                 <>
-                    <button className="join"
-                        onClick={() => {
-                            onClose()
-                            openSignup()
-                        }}
-                    >
-                        Join Giggler
+                    <button className="join" onClick={() => {
+                        onClose()
+                        openSignup()
+                    }}>
+                        Join
                     </button>
-                    <button className="login"
-                        onClick={() => {
-                            onClose()
-                            openLogin()
-                        }}
-                    >
+                    <button className="login" onClick={() => {
+                        onClose()
+                        openLogin()
+                    }}>
                         Sign In
                     </button>
-                    <Link to="/explore"> Explore </Link>
-                    <Link to="/">Become a Seller</Link>
+                    <Link to="/explore" onClick={onClose}> Explore </Link>
+                    <Link to="/" onClick={onClose}>Become a Seller</Link>
                 </>
             )}
         </div>
