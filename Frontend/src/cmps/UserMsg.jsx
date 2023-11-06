04fa624cffc212115ba53e9054c64abb3ca1803c
TAC42 <tomi.carlin@gmail.com>
2023-11-06 17:18:35 -0500

    created notification for seller/buyers flow using sockets

--- Frontend/src/cmps/UserMsg.jsx
+++ Frontend/src/cmps/UserMsg.jsx
@@ -37,10 +37,15 @@
 //     </section>
 //   )
 // }
-import { eventBusService } from "../services/event-bus.service.js"
+import {
+  eventBusService,
+  showErrorMsg,
+  showSuccessMsg,
+} from '../services/event-bus.service.js'
 import { useState, useEffect } from 'react'
 
-import SvgIcon from "./SvgIcon.jsx"
+import SvgIcon from './SvgIcon.jsx'
+import { socketService } from '../services/socket.service.js'
 
 export function UserMsg() {
   const [msg, setMsg] = useState(null)
@@ -48,7 +53,7 @@ export function UserMsg() {
   const [isSlidingOut, setIsSlidingOut] = useState(false)
 
   useEffect(() => {
-    const unsubscribe = eventBusService.on('show-user-msg', data => {
+    const unsubscribe = eventBusService.on('show-user-msg', (data) => {
       const { title, body, type, styles } = data
       setMsg({ title, body, type, styles })
 
@@ -65,6 +70,72 @@ export function UserMsg() {
     }
   }, [])
 
+  useEffect(() => {
+    socketService.on('notify-buyer-accepted', orderAcceptedNotification)
+    socketService.on('notify-buyer-denied', orderDeniedNotification)
+    socketService.on('notify-buyer-completed', orderCompletedNotification)
+    socketService.on('notify-seller-new-order', newOrderNotification)
+    return () => {
+      socketService.off('notify-buyer-accepted', orderAcceptedNotification)
+      socketService.off('notify-buyer-denied', orderDeniedNotification)
+      socketService.off('notify-buyer-completed', orderCompletedNotification)
+      socketService.off('notify-seller-new-order', newOrderNotification)
+    }
+  }, [])
+
+  function orderAcceptedNotification(user) {
+    showSuccessMsg(
+      {
+        title: 'ORDER ACCEPTED',
+        body: `${user.username} has accepted your order!`,
+      },
+      {
+        userMsgLeft: '55%',
+        messageAreaPadding: '2em 1.5em 2em 7em',
+        msgStatusTranslateX: '-12em',
+      }
+    )
+  }
+  function orderDeniedNotification(user) {
+    showErrorMsg(
+      {
+        title: 'ORDER DENIED',
+        body: `${user.username} has rejected your order!`,
+      },
+      {
+        userMsgLeft: '55%',
+        messageAreaPadding: '2em 1.5em 2em 7em',
+        msgStatusTranslateX: '-12em',
+      }
+    )
+  }
+  function orderCompletedNotification(user) {
+    showSuccessMsg(
+      {
+        title: 'ORDER COMPLETED',
+        body: `${user.username} has completed your order!`,
+      },
+      {
+        userMsgLeft: '55%',
+        messageAreaPadding: '2em 1.5em 2em 7em',
+        msgStatusTranslateX: '-12em',
+      }
+    )
+  }
+  function newOrderNotification(user) {
+    showSuccessMsg(
+      {
+        title: 'NEW ORDER',
+        body: `${user.username} just purchased a gig from you!`,
+      },
+      {
+        userMsgLeft: '55%',
+        messageAreaPadding: '2em 1.5em 2em 7em',
+        msgStatusTranslateX: '-12em',
+      }
+    )
+  }
+
   function onCloseMsg() {
     setIsSlidingOut(false)
     setMsg(null)
@@ -76,14 +147,24 @@ export function UserMsg() {
   const svgIconName = type === 'success' ? 'success' : 'error'
 
   return (
-    <section className={`user-msg ${isActive ? 'slide-in' : ''} ${isSlidingOut ? 'slide-out' : ''} ${type}`}
-      style={{ left: styles?.userMsgLeft }}>
-      <div className={`message-area flex column ${type}`} style={{ padding: styles?.messageAreaPadding }}>
+    <section
+      className={`user-msg ${isActive ? 'slide-in' : ''} ${
+        isSlidingOut ? 'slide-out' : ''
+      } ${type}`}
+      style={{ left: styles?.userMsgLeft }}
+    >
+      <div
+        className={`message-area flex column ${type}`}
+        style={{ padding: styles?.messageAreaPadding }}
+      >
         <button className={type} onClick={onCloseMsg}>
           <SvgIcon iconName={'remove'} />
         </button>
 
-        <div className={`msg-status flex row ${type}`} style={{ transform: `translateX(${styles?.msgStatusTranslateX})` }}>
+        <div
+          className={`msg-status flex row ${type}`}
+          style={{ transform: `translateX(${styles?.msgStatusTranslateX})` }}
+        >
           <SvgIcon iconName={svgIconName} />
 
           <p>{`${svgIconName}!`}</p>
