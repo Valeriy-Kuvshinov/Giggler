8b8c60d0f550eb4501a8841eaa685a39b1a4c43b
TAC42 <tomi.carlin@gmail.com>
2023-11-18 12:57:50 -0500

    working on mobile nav functionality and scss

--- Frontend/src/cmps/MobileFooter.jsx
+++ Frontend/src/cmps/MobileFooter.jsx
@@ -2,47 +2,55 @@ import { NavLink } from 'react-router-dom'
 import SvgIcon from './SvgIcon'
 import { MobileFilter } from './MobileFilter'
 import { useState } from 'react'
+import { useSelector } from 'react-redux'
 
 export function MobileFooter() {
+  const [navState, setNavState] = useState('home')
   const [search, setSearch] = useState(false)
 
   const loggedinUser = useSelector((storeState) => storeState.userModule.user)
 
-  function searchOn() {
+  function onOpenSearch() {
     setSearch((prevState) => !prevState)
   }
   return (
     <footer className="mobile-nav">
       <NavLink
         to="/"
-        exact="true"
-        className={(navData) => (navData.isActive ? 'active' : '')}
+        className={navState === 'home' ? 'selected' : ''}
+        onClick={() => setNavState('home')}
       >
         <SvgIcon iconName="appHomeIcon" />
       </NavLink>
       <NavLink
-        to="/messages"
-        className={(navData) => (navData.isActive ? 'active' : '')}
+        to={window.location.pathname}
+        className={navState === 'chat' ? 'selected' : ''}
+        onClick={() => setNavState('chat')}
       >
         <SvgIcon iconName="appEnvelopeIcon" />
       </NavLink>
       <NavLink
         to={window.location.pathname}
-        className={(navData) => (navData.isActive ? 'active' : '')}
-        onClick={() => openSearch()}
+        className={navState === 'search' ? 'selected' : ''}
+        onClick={(e) => {
+          setNavState('search')
+          onOpenSearch(e)
+        }}
       >
-        {searchOn && <MobileFilter />}
+        {search && <MobileFilter />}
         <SvgIcon iconName="appMagnifyingGlassIcon" />
       </NavLink>
       <NavLink
-        to="/orders"
-        className={(navData) => (navData.isActive ? 'active' : '')}
+        to={window.location.pathname}
+        className={navState === 'order' ? 'selected' : ''}
+        onClick={() => setNavState('order')}
       >
         <SvgIcon iconName="appOrdersIcon" />
       </NavLink>
       <NavLink
-        to="/profile"
-        className={(navData) => (navData.isActive ? 'active' : '')}
+        to={window.location.pathname}
+        className={navState === 'profile' ? 'selected' : ''}
+        onClick={() => setNavState('profile')}
       >
         <SvgIcon iconName="appProfileIcon" />
       </NavLink>
