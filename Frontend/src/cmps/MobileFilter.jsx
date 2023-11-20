150d68d8d53225f98de848368005be2fc77a50fc
TAC42 <tomi.carlin@gmail.com>
2023-11-20 03:53:50 -0500

    working on mobile-filter

--- Frontend/src/cmps/MobileFilter.jsx
+++ Frontend/src/cmps/MobileFilter.jsx
@@ -1,8 +1,18 @@
-export function MobileFilter(){
-    
-    return (
-        <section className="mobile-filter">
-            
-        </section>
-    )
+import SvgIcon from './SvgIcon'
+
+export function MobileFilter(filterBy) {
+  return (
+    <main className="mobile-filter">
+      <section className="mobile-filter-header">
+        <div className="wrapper">
+          <SvgIcon iconName={'remove'} />
+          <span>Filter all</span>
+        </div>
+        <span className="clear">Clear all</span>
+      </section>
+      <section className="filter-form">
+
+      </section>
+    </main>
+  )
 }
