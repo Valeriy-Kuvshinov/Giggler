13aca28ba38d2c95c871de006fdd88d92b2da6b9
Valeriy Kuvshinov <valerykuvshinuv@gmail.com>
2023-12-10 15:49:47 +0200

    buyer dashboard design finished

--- Frontend/src/cmps/BuyerOrder.jsx
+++ Frontend/src/cmps/BuyerOrder.jsx
@@ -12,7 +12,7 @@ export function BuyerOrder({ order, details, onClickReceipt, onClickReview, isFr
     return (
         <>
             {isFrom === 'orders' ? (
-                <div className={`buyer-order ${deviceType}`}>
+                <div className={`buyer-order ${deviceType} grid`}>
                     <Link
                         className="order-title"
                         to={`/gig/${details.gigData._id}`}
@@ -23,47 +23,45 @@ export function BuyerOrder({ order, details, onClickReceipt, onClickReview, isFr
                         <img src={details.gigData.imgUrls?.[0]} alt="Gig" />
                     </div>
 
-                    <div className="order-info flex column">
-                        <div className="order-id flex row">
-                            <span>ID:</span>
-                            <span>{order._id}</span>
-                        </div>
+                    <div className="order-id flex row">
+                        <span>ID:</span>
+                        <span>{order._id}</span>
+                    </div>
 
-                        <div className="category-name flex row">
-                            <span>Category:</span>
-                            <span>{details.gigData.category}</span>
-                        </div>
+                    <div className="category-name flex row">
+                        <span>Category:</span>
+                        <span>{details.gigData.category}</span>
+                    </div>
 
-                        <div className="seller-name flex row">
-                            <span>Seller name:</span>
-                            <span>
-                                <Link to={`/user/${details.userData._id}`}>
-                                    {`@${details.userData.username}`}
-                                </Link>
-                            </span>
-                        </div>
+                    <div className="seller-name flex row">
+                        <span>Seller:</span>
+                        <span>
+                            <Link to={`/user/${details.userData._id}`}>
+                                {`@${details.userData.username}`}
+                            </Link>
+                        </span>
+                    </div>
 
-                        <div className={`order-status ${order.orderState} flex row`}>
-                            <span>Order status:</span>
-                            <span className={`${order.orderState}`}>{order.orderState}</span>
-                        </div>
+                    <div className={`order-status ${order.orderState} flex row`}>
+                        <span>Status:</span>
+                        <span className={`${order.orderState}`}>{order.orderState}</span>
+                    </div>
 
-                        <div className="order-options flex row">
-                            {(order.orderState === 'accepted' ||
-                                order.orderState === 'completed' ||
-                                order.orderState === 'reviewed') && (
-                                    <span className="invoice-icon" title="Order Invoice"
-                                        onClick={(event) => onClickReceipt(event, order)}>
-                                        <SvgIcon iconName={'receiptIcon'} />
-                                    </span>
-                                )}
-                            {(order.orderState === 'completed') && (
-                                <span className="review-icon" title="Review Order"
-                                    onClick={(event) => onClickReview(event, order)}>
-                                    <SvgIcon iconName={'reviewIcon'} />
+                    <div className="order-options flex row">
+                        {(order.orderState === 'accepted' ||
+                            order.orderState === 'completed' ||
+                            order.orderState === 'reviewed') && (
+                                <span className="invoice-icon" title="Order Invoice"
+                                    onClick={(event) => onClickReceipt(event, order)}>
+                                    <SvgIcon iconName={'receiptIcon'} />
                                 </span>
                             )}
-                        </div>
+                        {(order.orderState === 'completed') && (
+                            <span className="review-icon" title="Review Order"
+                                onClick={(event) => onClickReview(event, order)}>
+                                <SvgIcon iconName={'reviewIcon'} />
+                            </span>
+                        )}
                     </div>
                 </div>
             ) : (
