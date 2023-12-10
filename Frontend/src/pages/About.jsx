c7cf6f749c02c55f0471c5adee0d32f5a1ed36be
RoeiTal123 <roeit1977@gmail.com>
2023-12-10 11:26:12 +0200

    start of about page

--- Frontend/src/pages/About.jsx
+++ Frontend/src/pages/About.jsx
@@ -1,7 +1,7 @@
 export function About() {
   return (
-    <main className="layout-row about-page">
-      about
+    <main className="flex column full about-page">
+      <h2>about our project:</h2>
       <img src="https://res.cloudinary.com/dgwgcf6mk/image/upload/v1701679834/Giggler/about-page/Screenshot_2023-12-04_104607_ecrlq9.png" className="profile-image"/>
     </main>
   )
